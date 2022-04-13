e95a4cb15c94e3a02c57e9ffa3a90a87a370b018
shello <marcello_menjivar@unitec.edu>
2022-04-13 12:59:56 -0600

    Actualizando el curso de Java Martes 12, 2022

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -20,10 +20,13 @@ public class HolaMundo {
         
         System.out.println(miVariableEntera);
         System.out.println("Hello World! You sexy beast! "+miVariableEntera);
-        System.out.printf("Hello you sexy beast %s \n", miVariableEntera);
+        System.out.printf("Hello you sexy beast %s,  ingresa un Mensaje para continuar: \n", miVariableEntera);
 
-        String miCadena = "Esta es una simple cadena de texto que encierra un gran mensaje.";
+        String miCadena = "Esta es una simple cadena de texto que encierra un gran mensaje. Enrolla la lengua, toca el cielo de la boca, \n "
+                + "concentra el Kundalini, y haz girar \n "
+                + "entonces entra el gargajo y te lo tragas. Enjoy. We exist. We don't exist.";
         
+       
         String segundaCadena = in.nextLine();
         
         System.out.println(miCadena);
@@ -56,12 +59,24 @@ public class HolaMundo {
         System.out.println("Valor Minimo de Long: "+ Long.MIN_VALUE);
         System.out.println("Valor Maximo de Long: "+ Long.MAX_VALUE);
         System.out.println("Valor de la variable numeroLong: "+ numeroLong);
+        
+        float numeroFloat = (float)3.4028235E38D;
+        
+        System.out.println("Valor Minimo de Float: "+ Float.MIN_VALUE);
+        System.out.println("Valor Maximo de Float: "+ Float.MAX_VALUE);
+        System.out.println("Valor de la variable numeroFloat: "+ numeroFloat);
+        
+        double numeroDouble = 1.7976931348623157E308D;
+        System.out.println("Valor Minimo de Double: "+ Double.MIN_VALUE);
+        System.out.println("Valor Maximo de Double: "+ Double.MAX_VALUE);
+        System.out.println("Valor de la variable numeroDouble: "+ numeroDouble);
+        
     }
     
     public static void imprimir(String temp){
         for(int i = 0 ; i < temp.length(); i++){
             System.out.println(temp.charAt(i));
-            try{Thread.sleep(150);}catch(InterruptedException e){System.out.println(e);};           
+            try{Thread.sleep(250);}catch(InterruptedException e){System.out.println(e);};           
         }
         System.out.println();
     }
